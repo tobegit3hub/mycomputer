6c7d30dd7f97019e55ba292f5521d05ab126e99c
tobe <tobeg3oogle@gmail.com>
2014-09-08 16:26:40 +0800

    use angular model for static personal page

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,19 @@
 function PhoneListCtrl($scope) {
   $scope.phones = [
     {"name": "Nexus S",
+     "image_url": "macbook_air.jpeg",
      "snippet": "Fast just got faster with Nexus S."},
     {"name": "Motorola XOOM™ with Wi-Fi",
+     "image_url": "miwifi.jpg",
      "snippet": "The Next, Next Generation tablet."},
     {"name": "MOTOROLA XOOM™",
+     "image_url": "newbalance.jpg",
      "snippet": "The Next, Next Generation tablet."}
   ];
 }
+
 /*
-var app = angular.module("MyApp", []);
+var app = angular.module("app", []);
 
 app.controller("PhoneListCtrl", function($scope, $http) {
   $http.get('https://raw.githubusercontent.com/tobegit3hub/mycomputer/master/phones.json').
