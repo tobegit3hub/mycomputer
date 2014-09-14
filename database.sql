d565b3c3327c7335e92894ae22f2d8f4ec8c4670
tobe <tobeg3oogle@gmail.com>
2014-09-14 17:32:51 +0800

    make MySQL support utf-8

--- database.sql
+++ database.sql
@@ -1,21 +1,33 @@
-CREATE Table mycomputer;
+/* Make MySQL support utf-8
+root@ emacs /etc/mysql/my.cnf
+[client]
+default-character-set=utf8
+[mysqld]
+init_connect=’SET collation_connection = utf8_unicode_ci’
+init_connect=’SET NAMES utf8′
+character-set-server=utf8
+collation-server=utf8_unicode_ci
+skip-character-set-client-handshake
+*/
+
+CREATE DATABASE mycomputer;
 
 USE mycomputer;
 
 CREATE TABLE IF NOT EXISTS user (
-    name varchar(256) PRIMARY KEY,
-    email varchar(256),
-    description varchar(256)
+    name varchar(255) PRIMARY KEY,
+    email varchar(255),
+    description varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS item (
    number int PRIMARY KEY,
-   image varchar(256) NOT NULL,
-   description varchar(256),
-   username varchar(256) REFERENCES user(name)
+   image varchar(255),
+   description varchar(255),
+   username varchar(255) REFERENCES user(name)
 );
 
 CREATE TABLE IF NOT EXISTS comment (
-       id int PRIMARY KEY AUTO_INCREMENT,
-       content varchar(256)
+  id int PRIMARY KEY AUTO_INCREMENT,
+  content varchar(255)
 )
