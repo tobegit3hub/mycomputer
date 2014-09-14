ba59fe798bacac7a60ca243c9d5af499f086ca98
tobe <tobeg3oogle@gmail.com>
2014-09-14 15:57:59 +0800

    get data from beego api

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,24 +1,22 @@
 'use strict';
 
-/* Controllers */
-
+/* The angular application controllers */
 var mycomputerControllers = angular.module('mycomputerControllers', []);
 
+/* Todo: add comment */
 mycomputerControllers.controller('HomeController', ['$scope',
   function($scope) {
+}]);
 
-  }]);
-
-
+/* This controller to get user and items from mycomputer beego api */
 mycomputerControllers.controller('UserItemsController', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
-//    $http.get('/api/' + $routeParams.username).success(function(data) {
-    $http.get('/static/' + $routeParams.username + ".json").success(function(data) {
-      $scope.phones = data;
+    $http.get('/api/' + $routeParams.username).success(function(data) {
+      $scope.items = data;
     });
 
-/*    $http.get('static/' + $routeParams.username +'.json').success(function(data) {
+/*    $http.get('api/' + $routeParams.username +'/items').success(function(data) {
       $scope.user = data;
-    });
-*/
+    });*/
+
   }]);
